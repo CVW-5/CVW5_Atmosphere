942bee98bb88fd006eaed5a499dea22ce9a4e8de
Dan Lodholm <dlodholm@icloud.com>
2023-01-03 16:39:25 -0700

    ReferenceAltitude struct and reference data file

---
+++ CVW5_Atmosphere/ReferenceAltitude.cs
@@ -0,0 +1,15 @@
+﻿namespace CVW5_Atmosphere
+{
+    internal struct ReferenceAltitude
+    {
+        internal float Altitude, Pressure, Temp, TempLapseRate;
+
+        internal ReferenceAltitude(float alt, float pres, float temp, float lapse)
+        {
+            Altitude = alt;
+            Pressure = pres;
+            Temp = temp;
+            TempLapseRate = lapse;
+        }
+    }
+}
