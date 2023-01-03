7d06f7d946bac9f6ac4b859d50d6429f64b45564
Dan Lodholm <dlodholm@icloud.com>
2023-01-03 15:19:40 -0700

    Add project files.

---
+++ CVW5_Atmosphere/Atmosphere.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CVW5_Atmosphere
+{
+    public class Atmosphere
+    {
+        public const float Gravity = 9.80665f;
+        public const float MolarMass = 0.0289644f;
+        public const float GasConst = 8.3144598f;
+
+
+    }
+}
