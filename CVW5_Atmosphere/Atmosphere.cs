b4b689446a5f22abb4f4e2da5cf7198df4d9e7e0
Dan Lodholm <dlodholm@icloud.com>
2023-01-03 18:31:11 -0700

    Block comment with important details for Atmosphere.cs

--- CVW5_Atmosphere/Atmosphere.cs
+++ CVW5_Atmosphere/Atmosphere.cs
@@ -1,10 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text.RegularExpressions;
+﻿/** +===========================================================+
+ *  |         ATMOSPHERE OBJECT - CVW5_Atmosphere               |
+ *  |         Author: Dani Lodholm (Sendit/OrbitusII)           |
+ *  |         Written: January 2023                             |
+ *  |                                                           |
+ *  |     This is the basic unit of functionality within this   |
+ *  | library. Without it, you do nothing.                      |
+ *  |                                                           |
+ *  | Atmosphere.cs is a partial class. See Atmosphere.*.cs for |
+ *  | other relevant functionality like solvers, constants, and |
+ *  | initializers/constructors.                                |
+ *  |                                                           |
+ *  | Basic behavior is based on the US Standard Atmosphere and |
+ *  | Barometric formula, see the links below:                  |
+ *  | https://en.wikipedia.org/wiki/U.S._Standard_Atmosphere    |
+ *  | https://en.wikipedia.org/wiki/Barometric_formula          |
+ *  +===========================================================+
+ */
 
 namespace CVW5_Atmosphere
 {
+    /// <summary>
+    /// The base Atmosphere object. Contains all necessary data and methods for atmospheric effects.
+    /// </summary>
     public partial class Atmosphere
     {
         public string AtmosphereType { get; set; } = "Unknown";
