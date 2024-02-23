7b278a1e114ccf129d42ff19e05302404e0e730a
Robin Lodholm <dlodholm@icloud.com>
2024-02-22 23:54:09 -0700

    Move Atmosphere.US_Standard into Atmosphere.cs

--- CVW5_Atmosphere/Atmosphere.cs
+++ CVW5_Atmosphere/Atmosphere.cs
@@ -29,5 +29,22 @@ namespace CVW5_Atmosphere
         public ReferenceAltitude[] RefAltitudes { get; internal set; } = new ReferenceAltitude[0];
 
         private Atmosphere() { }
+
+        public static Atmosphere US_Standard => new Atmosphere()
+        {
+            RefAltitudes = new ReferenceAltitude[]
+            {
+                new ReferenceAltitude(0, 101325, 288.15f, -0.0065f),
+                new ReferenceAltitude(11000, 22632.1f, 216.65f, 0),
+                new ReferenceAltitude(20000, 5474.89f, 216.65f, 0.001f),
+                new ReferenceAltitude(32000,868.019f,228.65f,0.0028f ),
+                new ReferenceAltitude(47000,110.906f,270.65f,0),
+                new ReferenceAltitude(51000,66.9389f,270.65f,-0.0028f),
+                new ReferenceAltitude(71000,3.95642f,214.65f,-0.002f),
+                new ReferenceAltitude(84852,0,187.15f,0)
+            },
+            Year = 1976,
+            AtmosphereType = "US Standard Atmosphere",
+        };
     }
 }
